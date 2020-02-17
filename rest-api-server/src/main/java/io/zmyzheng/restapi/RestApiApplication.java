25bdfdf54a42a65ad00e8e81622cb7426523cb9e
Mingyang Zheng <zhengzmy@gmail.com>
2020-02-16 21:30:34 -0800

    initial commit for rest-api

---
+++ rest-api-server/src/main/java/io/zmyzheng/restapi/RestApiApplication.java
@@ -0,0 +1,13 @@
+package io.zmyzheng.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiApplication.class, args);
+	}
+
+}
---
+++ rest-api-server/src/test/java/io/zmyzheng/restapi/RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.zmyzheng.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
