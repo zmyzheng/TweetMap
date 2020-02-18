76c48bb7b161f3f7d464a529d0e3f9800afd3ece
Mingyang Zheng <zhengzmy@gmail.com>
2020-02-17 18:48:57 -0800

    build info

---
+++ rest-api-server/src/main/java/io/zmyzheng/restapi/controller/TweetController.java
@@ -0,0 +1,11 @@
+package io.zmyzheng.restapi.controller;
+
+/**
+ * @Author: Mingyang Zheng
+ * @Date: 2020-02-17 17:53
+ */
+
+
+public class TweetController {
+
+}
---
+++ rest-api-server/src/main/java/io/zmyzheng/restapi/service/TweetService.java
@@ -0,0 +1,8 @@
+package io.zmyzheng.restapi.service;
+
+/**
+ * @Author: Mingyang Zheng
+ * @Date: 2020-02-17 17:54
+ */
+public interface TweetService {
+}
