bd8f69164ab09dfbd5d2db1d75633e126391e185
Mingyang Zheng <zhengzmy@gmail.com>
2021-01-17 17:18:10 -0800

    TrendController: filterTrendByTopic

--- rest-api-server/src/main/java/io/zmyzheng/restapi/api/controller/TrendController.java
+++ rest-api-server/src/main/java/io/zmyzheng/restapi/api/controller/TrendController.java
@@ -1,8 +1,14 @@
 package io.zmyzheng.restapi.api.controller;
 
+import io.zmyzheng.restapi.api.mapping.TopicTrendMapper;
+import io.zmyzheng.restapi.api.model.TopicTrendDTO;
+import io.zmyzheng.restapi.api.model.TopicTrendFilter;
+import io.zmyzheng.restapi.service.TopicTrendService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @Author Mingyang Zheng
@@ -17,5 +23,18 @@ public class TrendController {
 
     public static final String BASE_URL = "api/v3/trends";
 
+    private final TopicTrendService topicTrendService;
+    private final TopicTrendMapper topicTrendMapper;
 
+    public TrendController(TopicTrendService topicTrendService, TopicTrendMapper topicTrendMapper) {
+        this.topicTrendService = topicTrendService;
+        this.topicTrendMapper = topicTrendMapper;
+    }
+
+    @PostMapping("/topic")
+    @ResponseStatus(HttpStatus.OK)
+    public List<TopicTrendDTO> filterTrendByTopic(@RequestBody TopicTrendFilter topicTrendFilter) {
+        return this.topicTrendMapper
+                .convert(this.topicTrendService.getTopicTrend(topicTrendFilter.getTopicName(), topicTrendFilter.getCalendarInterval(), topicTrendFilter.getTimeFrom(), topicTrendFilter.getTimeTo(), topicTrendFilter.getCenter(), topicTrendFilter.getRadius()));
+    }
 }
