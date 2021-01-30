1705f989f58641bda6c76a1bb0ada6e9e27579f8
Mingyang Zheng <zhengzmy@gmail.com>
2021-01-30 15:57:10 -0800

    add nav and footer

--- tweet-map-frontend/src/app/app.module.ts
+++ tweet-map-frontend/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
