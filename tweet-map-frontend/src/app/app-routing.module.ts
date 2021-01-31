e706449f7e9faf80fe47afa09e71ac1c4f185049
Mingyang Zheng <zhengzmy@gmail.com>
2021-01-30 17:46:12 -0800

    add route

--- tweet-map-frontend/src/app/app-routing.module.ts
+++ tweet-map-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MapComponent} from './map/map.component';
+import {TopicListComponent} from './topic-list/topic-list.component';
+import {TrendComponent} from './trend/trend.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/map', pathMatch: 'full' },
+  { path: 'map', component: MapComponent },
+  { path: 'topics', component: TopicListComponent },
+  { path: 'trend', component: TrendComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
